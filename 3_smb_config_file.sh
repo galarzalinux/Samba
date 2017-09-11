6df491fa4634ce4ea6dc34a728f7f8a23c0cc856
Jose Arturo Medina Galarza <galarzaingeniero@gmail.com>
2017-09-11 11:19:19 -0500

    Update 3_smb_config_file.sh

--- 3_smb_config_file.sh
+++ 3_smb_config_file.sh
@@ -13,41 +13,24 @@ echo "Inserting settings to the file smb.conf"
 cd /etc/samba/
 sudo echo "#======================= Global Settings ===================================== ">>smb.conf
 sudo echo "[global]">>smb.conf
-sudo echo "workgroup = AMR">>smb.conf
+#sudo echo "workgroup = MI CASA">>smb.conf
 sudo echo "server string = Samba Server %v">>smb.conf
-sudo echo "netbios name = GalarzaServer">>smb.conf
+sudo echo "netbios name = Galarza_RPi">>smb.conf
 sudo echo "security = user">>smb.conf
+sudo echo "encrypt passwords = yes">>smb.conf
 sudo echo "username map = /etc/samba/smbusers">>smb.conf
 sudo echo "map to guest = bad user">>smb.conf
-#sudo echo "dns proxy = http://proxy-chain.intel.com:911">>smb.conf
 sudo echo "dns proxy = no">>smb.conf
-sudo echo "#============================ Share Definitions ============================== ">>smb.conf
-sudo echo "[Share Folder]">>smb.conf
-sudo echo "path = /srv/samba/share">>smb.conf
+sudo echo "#=============================== Secure User ================================= ">>smb.conf
+sudo echo "[Galarza Secure]">>smb.conf
+sudo echo "path = ~/My_Drive/">>smb.conf
+#sudo echo "valid users = @users">>smb.conf
+#sudo echo "force group = users">>smb.conf
 sudo echo "browsable =yes">>smb.conf
-sudo echo "guest ok = yes">>smb.conf
+sudo echo "guest ok = no">>smb.conf
 sudo echo "read only = no">>smb.conf
 sudo echo "writable = yes">>smb.conf
+sudo echo "public = no">>smb.con
 sudo echo "create mask = 0777">>smb.conf
 sudo echo "directory mask = 0777">>smb.conf
-sudo echo "#=============================== Secure User ================================= ">>smb.conf
-sudo echo "[BBUX Folder]">>smb.conf
-sudo echo "path = /srv/samba/bbuxshare">>smb.conf
-sudo echo "valid users = bbux">>smb.conf
-sudo echo "browsable =yes">>smb.conf
-sudo echo "guest ok = no">>smb.conf
-sudo echo "read only = no">>smb.conf
-sudo echo "writable = yes">>smb.conf
-sudo echo "create mask = 0750">>smb.conf
-sudo echo "directory mask = 0750">>smb.conf
-sudo echo "#============================== Secure User 2================================= ">>smb.conf
-sudo echo "[Debug Folder]">>smb.conf
-sudo echo "path = /srv/samba/debugshare">>smb.conf
-sudo echo "valid users = debug">>smb.conf
-sudo echo "browsable =yes">>smb.conf
-sudo echo "guest ok = no">>smb.conf
-sudo echo "read only = no">>smb.conf
-sudo echo "writable = yes">>smb.conf
-sudo echo "create mask = 0750">>smb.conf
-sudo echo "directory mask = 0750">>smb.conf
 echo "################################################################################################"
