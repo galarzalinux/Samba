5b94fc88c4dde31a3e22b791569bba73789b5861
Jose Arturo Medina Galarza <galarzaingeniero@gmail.com>
2017-09-11 11:09:15 -0500

    Update 1_package_installer.sh

--- 1_package_installer.sh
+++ 1_package_installer.sh
@@ -6,12 +6,16 @@ fi
 echo "################################################################################################"
 echo "Installing Samba and aditional packages"
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install openssh-server
 sudo apt-get install samba samba
 sudo apt-get install samba samba-common
 sudo apt-get install samba smbfs
 sudo apt-get install libpam-smbpass
 sudo apt-get install python-glade2 system-config-samba
-#sudo apt-get upgrade
+sudo apt-get clean
+sudo apt-get remove
+sudo apt-get autoclean
+sudo apt-get autoremove
 echo "End of installation"
 echo "################################################################################################"
