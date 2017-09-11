c6e315be463630a468c0e483e77944c089c859a2
Jose Arturo Medina Galarza <galarzaingeniero@gmail.com>
2017-09-11 11:16:41 -0500

    Update 2_user_and_folder_creator.sh

--- 2_user_and_folder_creator.sh
+++ 2_user_and_folder_creator.sh
@@ -5,27 +5,9 @@ exit 0
 fi
 echo "################################################################################################"
 echo "Creating Directory for share"
-mkdir -p /srv/samba/share
-mkdir -p /srv/samba/bbuxshare
-mkdir -p /srv/samba/debugshare
-sudo chown nobody.nogroup /srv/samba/share/
-echo "################################################################################################"
-echo "Creating First User"
-sudo adduser bbux
-sudo addgroup bbuxgroup
-sudo adduser bbux bbuxgroup
-sudo chown -R bbux:bbuxgroup /srv/samba/bbuxshare/
-sudo smbpasswd -a bbux
-echo "Creating Second User"
-sudo adduser debug
-sudo addgroup debuggroup
-sudo adduser debug debuggroup
-sudo chown -R debug:debuggroup /srv/samba/debugshare/
-sudo smbpasswd -a debug
+mkdir -p ~/My_Drive/
 echo "################################################################################################"
 echo "Making the Directory Writable"
-cd /srv/samba/
-sudo chmod -R 0777 share
-sudo chmod -R 0770 bbuxshare
-sudo chmod -R 0770 debugshare
+cd ~/
+chmod -R 0777 ~/My_Drive/
 echo "################################################################################################"
